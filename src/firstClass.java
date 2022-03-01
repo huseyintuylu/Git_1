7ee5be76b4fa7fa67588168da36627f3be853bc3
huseyintuylu <100725326+huseyintuylu@users.noreply.github.com>
2022-03-01 21:32:48 +0100

    Update firstClass.java

--- src/firstClass.java
+++ src/firstClass.java
@@ -1,5 +1,6 @@
 public class firstClass {
     public static void main(String[] args) {
         System.out.println("Her Hayrin Basi Bismillah");
+        System.out.println("Her Hayrin Sonu Bismillah");
     }
 }
